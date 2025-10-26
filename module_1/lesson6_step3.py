c9912f449d526e1d4a7187243ac352312373b97e
Kefir7 <2005iaro@gmail.com>
2025-10-26 18:36:17 +0500

    add module_1

---
+++ module_1/lesson6_step3.py
@@ -0,0 +1,30 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+
+
+# link = "http://suninjuly.github.io/simple_form_find_task.html"
+# browser = webdriver.Chrome()
+# browser.get(link)
+# button = browser.find_element(By.ID, "submit_button")
+# button.click()
+
+# # закрытие браузера
+# browser.quit()
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://suninjuly.github.io/simple_form_find_task.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    time.sleep(3)
+    button = browser.find_element(By.ID, "submit_button")
+    time.sleep(3)
+    button.click()
+
+finally:
+    # закрываем браузер после всех манипуляций
+    browser.quit()
